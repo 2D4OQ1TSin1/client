c7d999eb5ae89f4ff778baeec93d948f10223450
andrew <andrew@bobux.com>
2024-04-18 01:36:32 -0400

    2024-04-18 ~ 1:36AM

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Trending from "../Components/Trending/Trending";
+import NewArrivals from "../Components/NewArrivals/NewArrivals";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
       <Trending />
+      <NewArrivals />
     </div>
   );
 };
