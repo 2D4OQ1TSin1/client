bb944eaf67921139daa75a4f790527a0637fb7c2
andrew <andrew@bobux.com>
2024-04-18 00:45:10 -0400

    2024-04-18 ~ 12:45AM

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./Hero.css";
 import hero_image from "../Assets/hero_image.png";
+import hand_icon from "../Assets/electricity.svg";
+import arrow_icon from "../Assets/arrow.png";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>HIGH IN-DEMAND ONLY</h2>
+        <h2>WE GOT IT ALL</h2>
         <div>
-          <div className="hand-hand-icon">
-            <p>hardware</p>
+          <div className="hero-hand-icon">
+            <p>cpu</p>
+            <img src={hand_icon} alt="" width={100} />
           </div>
-          <p>collection</p>
-          <p>for everyone</p>
+          <p>memory</p>
+          <p>graphic cards</p>
         </div>
         <div className="hero-latest-btn">
-          <div></div>
+          <div>View Offers</div>
+          <img src={arrow_icon} alt="" width={50} />
         </div>
       </div>
       <div className="hero-right">
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
+import Trending from "../Components/Trending/Trending";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Trending />
     </div>
   );
 };
